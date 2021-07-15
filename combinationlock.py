e319bec2627e191f2186bc253ec975095053d95d
William Dizon <wdchromium@gmail.com>
2021-07-15 12:56:00 -0700

    Testdriven (#1)
    * added button push for travel distances
    
    * added functionality to return entered code
    
    * first draft tdd combo lock
    
    * lock opens when combo pattern entered
    
    * set default, but invalid 0,0,0 code to remove try blocks
    
    * more tests to show always locked until proper code
    
    * removed old combolock version

---
+++ combinationlock.py
@@ -0,0 +1,62 @@
+# Author: William Dizon <wdchromium@gmail.com>
+
+from enum import Enum
+
+class LockInputs(Enum):
+    Anticlockwise = 0
+    Clockwise = 1
+    OpenShackle = 2
+    CloseShackle = 3
+
+class CombinationLock(object):
+    
+    DIAL_SIZE = 94
+    COMBO_LENGTH = 3
+
+    @classmethod
+    def distance(cls, start_pos, end_pos, direction):
+        if direction is LockInputs.Clockwise:
+            if start_pos < end_pos: #short distance
+                return end_pos - start_pos
+            else: #long distance
+                return cls.DIAL_SIZE - (start_pos - end_pos)
+        elif direction is LockInputs.Anticlockwise:
+            if start_pos < end_pos: # long distance
+                return start_pos + (cls.DIAL_SIZE - end_pos)
+            else:
+                return start_pos - end_pos
+
+    def __init__(self, code=(0,0,0)):
+        from collections import deque
+
+        self.position = 0
+        self.secured = True
+        self.code = code
+        self.code_sequence = []
+        self.movement = []
+
+        for i in range(self.distance(code[0], code[1], LockInputs.Anticlockwise) + self.DIAL_SIZE):
+            self.code_sequence.append(LockInputs.Anticlockwise)
+        for i in range(self.distance(code[1], code[2], LockInputs.Clockwise)):
+            self.code_sequence.append(LockInputs.Clockwise)
+
+    def interact(self, inputaction):
+        if inputaction is LockInputs.Clockwise:
+            self.position += 1
+            self.movement.append(inputaction)
+        elif inputaction is LockInputs.Anticlockwise:
+            self.position -= 1
+            self.movement.append(inputaction)
+
+        if self.position < 0:
+            self.position = self.DIAL_SIZE - 1
+        elif self.position == self.DIAL_SIZE:
+            self.position = 0
+
+        from itertools import zip_longest, chain, repeat
+        iter_movement = iter(self.movement)
+        iter_code = chain(repeat(LockInputs.Clockwise, self.code[0]), iter(self.code_sequence))
+        zipped = zip_longest(iter_code, iter_movement)
+
+        self.secured = not all(a == b for a,b in zipped)
+
---
+++ test_combo.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import unittest
+import combinationlock
+
+class TestComboLock(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_consts(self):
+        inst = combinationlock.CombinationLock()
+        self.assertEqual(inst.DIAL_SIZE, 94)
+        self.assertEqual(inst.COMBO_LENGTH, 3)
+
+    def test_default_pos(self):
+        inst = combinationlock.CombinationLock()
+        self.assertEqual(inst.position, 0)
+
+    def test_rotate(self):
+        inst = combinationlock.CombinationLock()
+        li = combinationlock.LockInputs
+
+        inst.interact(li.Clockwise)
+        self.assertEqual(inst.position, 1)
+        inst.interact(li.Anticlockwise)
+        self.assertEqual(inst.position, 0)
+        inst.interact(li.Anticlockwise)
+        self.assertEqual(inst.position, inst.DIAL_SIZE - 1)
+        inst.interact(li.Clockwise)
+        self.assertEqual(inst.position, 0)
+        inst.interact(li.Clockwise)
+        self.assertEqual(inst.position, 1)
+
+    def test_secured(self):
+        inst = combinationlock.CombinationLock()
+        li = combinationlock.LockInputs
+
+        inst.interact(li.CloseShackle)
+        self.assertTrue(inst.secured)
+        inst.interact(li.OpenShackle)
+        self.assertTrue(inst.secured)
+
+    def test_set_code(self):
+        inst = combinationlock.CombinationLock()
+        self.assertTrue(inst.secured)
+        self.assertEqual(inst.code, (0,0,0))
+
+        inst = combinationlock.CombinationLock( (36,24,36) )
+        self.assertTrue(inst.secured)
+        self.assertEqual(inst.code, (36,24,36))
+
+        inst = combinationlock.CombinationLock( (30,80,12) )
+        self.assertTrue(inst.secured)
+        self.assertEqual(inst.code, (30,80,12))
+
+    def test_code_sequence(self):
+        inst = combinationlock.CombinationLock( (36,24,36) )
+        seq = iter(inst.code_sequence)
+        self.assertEqual(len(inst.code_sequence), 118)
+
+        inst = combinationlock.CombinationLock( (30,80,12) )
+        seq = iter(inst.code_sequence)
+        self.assertEqual(len(inst.code_sequence), 164)
+
+    def test_distance(self):
+        clsm = combinationlock.CombinationLock
+        li = combinationlock.LockInputs
+
+        self.assertEqual(clsm.distance(0, 10, li.Clockwise), 10)
+        self.assertEqual(clsm.distance(0, 10, li.Anticlockwise), 84)
+
+        self.assertEqual(clsm.distance(0, 90, li.Clockwise), 90)
+        self.assertEqual(clsm.distance(0, 90, li.Anticlockwise), 4)
+
+        self.assertEqual(clsm.distance(30, 50, li.Clockwise), 20)
+        self.assertEqual(clsm.distance(30, 50, li.Anticlockwise), 74)
+
+        self.assertEqual(clsm.distance(30, 20, li.Clockwise), 84)
+        self.assertEqual(clsm.distance(30, 20, li.Anticlockwise), 10)
+
+    def test_open_lock(self):
+        inst = combinationlock.CombinationLock( (36,24,36) )
+        li = combinationlock.LockInputs
+
+        self.assertTrue(inst.secured)
+
+        for _ in range(36):
+            inst.interact(li.Clockwise)
+            self.assertTrue(inst.secured)
+
+        for _ in range(inst.DIAL_SIZE + inst.distance(36, 24, li.Anticlockwise)):
+            inst.interact(li.Anticlockwise)
+            self.assertTrue(inst.secured)
+
+        for _ in range(inst.distance(24, 36, li.Clockwise) - 1):
+            inst.interact(li.Clockwise)
+            self.assertTrue(inst.secured)
+
+        inst.interact(li.Clockwise)
+        self.assertFalse(inst.secured)
+
+    def test_unopened_lock_relocks(self):
+        inst = combinationlock.CombinationLock( (36,24,36) )
+        li = combinationlock.LockInputs
+
+        self.assertTrue(inst.secured)
+
+        for _ in range(36):
+            inst.interact(li.Clockwise)
+            self.assertTrue(inst.secured)
+
+        for _ in range(inst.DIAL_SIZE + inst.distance(36, 24, li.Anticlockwise)):
+            inst.interact(li.Anticlockwise)
+            self.assertTrue(inst.secured)
+
+        for _ in range(inst.distance(24, 36, li.Clockwise) - 1):
+            inst.interact(li.Clockwise)
+            self.assertTrue(inst.secured)
+
+        inst.interact(li.Clockwise)
+        self.assertFalse(inst.secured)
+        inst.interact(li.Clockwise)
+        self.assertTrue(inst.secured)
+
+    def test_unset_lock(self):
+        inst = combinationlock.CombinationLock()
+        li = combinationlock.LockInputs
+
+        self.assertTrue(inst.secured)
+        # this works because of the implied +DIAL_SIZE for the anticlock rotation
+        for _ in range(inst.DIAL_SIZE):
+            inst.interact(li.Anticlockwise)
+            self.assertTrue(inst.secured)
+        self.assertTrue(inst.secured)
+
+if __name__ == "__main__":
+    unittest.main()
+
